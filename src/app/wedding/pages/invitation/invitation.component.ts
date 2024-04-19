dd327b45cfa25e044745c68cdadbf81b703f579a
CarlosRDZCas <carlosdanielrdz@hotmail.com>
2024-04-19 11:46:09 -0500

    confirmation

--- src/app/wedding/pages/invitation/invitation.component.ts
+++ src/app/wedding/pages/invitation/invitation.component.ts
@@ -1,11 +1,35 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import data from '../../data/invitados.json';
 
 @Component({
   selector: 'page-invitation',
 
   templateUrl: './invitation.component.html',
   styleUrls: ['./invitation.component.css'],
-
 })
-export class InvitationComponent { }
+export class InvitationComponent implements AfterViewInit {
+  param: string = '';
+  json: string = '';
+  invitados: string[] = [];
+  existe: string = '';
+  isLoading: boolean = true;
+
+  constructor(private route: ActivatedRoute) {
+    this.param = this.route.snapshot.queryParamMap.get('ID')!;
+    //  this.existe = data.find((ID)=>ID.ID===this.param)!
+    // console.log(this.existe);
+    let invitado = data.filter((ID) => ID.ID === this.param);
+    invitado.forEach((element) => {
+      this.existe = element.ID;
+    });
+  }
+  ngAfterViewInit(): void {
+    this.isLoading = false;
+  }
+}
